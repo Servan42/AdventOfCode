04a040557de634dbaf46737c6b77cecc6ce5ad82
Servan42 <charlot.servan@gmail.com>
2023-12-05 00:11:35 +0100

    feat: Day4

---
+++ AdventOfCode2023.Core/Daily/Day4/Card.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.Core.Daily.Day4
+{
+    public class Card
+    {
+        public static Card Parse(string inputLine)
+        {
+            var card = new Card();
+            
+            var cardNumbers = card.GetCardNumbers(inputLine);
+            var myNumbers = card.GetMyNumbers(inputLine);
+            card.NbOfWinningNumbers = myNumbers.Count(n => cardNumbers.Contains(n));
+            card.Amount = 1;
+            
+            return card;
+        }
+
+        public int NbOfWinningNumbers { get; set; }
+        public int Amount { get; set; }
+
+        public int CalculatePoints()
+        {
+            return (int) Math.Pow(2, NbOfWinningNumbers - 1);
+        }
+
+        private List<int> GetMyNumbers(string line)
+        {
+            return ReturnUsefulNumbers(line, 1);
+        }
+
+        private List<int> GetCardNumbers(string line)
+        {
+            return ReturnUsefulNumbers(line, 0);
+        }
+
+        private List<int> ReturnUsefulNumbers(string line, int positionToPipe)
+        {
+            return line
+                .Split(':')[1]
+                .Split('|')[positionToPipe]
+                .Split(' ')
+                .Where(n => !string.IsNullOrEmpty(n))
+                .Select(n => int.Parse(n))
+                .ToList();
+        }
+    }
+}
---
+++ AdventOfCode2023.Core/Daily/Day4/Day4Ex.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.Core.Daily.Day4
+{
+    public class Day4Ex : Exercise
+    {
+        public override void ComputePart1()
+        {
+            Output = InputLines
+                .Select(line => Card.Parse(line).CalculatePoints())
+                .Sum()
+                .ToString();
+        }
+
+        public override void ComputePart2()
+        {
+            var cards = new List<Card>();
+            foreach (var line in InputLines)
+            {
+                cards.Add(Card.Parse(line));
+            }
+
+            int nextCardIndex = 0;
+            foreach(var card in cards)
+            {
+                nextCardIndex++;
+                AddPlusXToNextYCardsAmount(card.NbOfWinningNumbers, nextCardIndex, card.Amount, cards);
+            }
+
+            Output = cards.Sum(c => c.Amount).ToString();
+        }
+
+        private void AddPlusXToNextYCardsAmount(int nbOfCardsToUp, int fromIndex, int amountToAdd, List<Card> cards)
+        {
+            for(int i = fromIndex; i < Math.Min(fromIndex + nbOfCardsToUp, cards.Count); i++)
+            {
+                cards[i].Amount += amountToAdd;
+            }
+        }
+    }
+}
