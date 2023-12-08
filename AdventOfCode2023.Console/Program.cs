356d40dc44a072711ede083527d3ed5cfd0f8eb8
Servan CHARLOT <charlot.servan@gmail.com>
2023-12-08 01:10:25 +0100

    feat: Day7
    * feat: Day7 PART1
    * feat: Day7 PART2

--- AdventOfCode2023.Console/Program.cs
+++ AdventOfCode2023.Console/Program.cs
@@ -1,4 +1,4 @@
-﻿using AdventOfCode2023.Core.Daily.Day6;
+﻿using AdventOfCode2023.Core.Daily.Day7;
 
 internal class Program
 {
@@ -7,7 +7,7 @@ internal class Program
     /// </summary>
     private static void Main(string[] args)
     {
-        var exercise = new Day6Ex();
+        var exercise = new Day7Ex();
         exercise.LoadInputFromFile("input.txt");
         exercise.ComputePart2();
         Console.WriteLine(exercise.Output);
