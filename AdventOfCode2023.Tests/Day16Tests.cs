04de02ae28ff6853fd781726bdb8d1dc33f01816
Servan42 <charlot.servan@gmail.com>
2023-12-16 19:24:13 +0100

    feat: Day16

---
+++ AdventOfCode2023.Tests/Day16Tests.cs
@@ -0,0 +1,70 @@
+﻿using AdventOfCode2023.Core.Daily.Day16;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.Tests
+{
+    internal class Day16Tests
+    {
+        Day16Ex sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            sut = new Day16Ex();
+        }
+
+        [Test]
+        public void Should_part1()
+        {
+            // GIVEN
+            sut.InputLines = new List<string>()
+            {
+                @".|...\....",
+                @"|.-.\.....",
+                @".....|-...",
+                @"........|.",
+                @"..........",
+                @".........\",
+                @"..../.\\..",
+                @".-.-/..|..",
+                @".|....-|.\",
+                @"..//.|...."
+            };
+
+            // WHEN
+            sut.ComputePart1();
+
+            // THEN
+            Assert.That(sut.Output, Is.EqualTo("46"));
+        }
+
+        [Test]
+        public void Should_part2()
+        {
+            // GIVEN
+            sut.InputLines = new List<string>()
+            {
+                @".|...\....",
+                @"|.-.\.....",
+                @".....|-...",
+                @"........|.",
+                @"..........",
+                @".........\",
+                @"..../.\\..",
+                @".-.-/..|..",
+                @".|....-|.\",
+                @"..//.|...."
+            };
+
+            // WHEN
+            sut.ComputePart2();
+
+            // THEN
+            Assert.That(sut.Output, Is.EqualTo("51"));
+        }
+    }
+}
