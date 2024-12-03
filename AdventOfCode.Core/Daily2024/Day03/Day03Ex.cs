0b0c7d1eb22fc4c12ba51cae0855bb0f868fddc6
Servan42 <charlot.servan@gmail.com>
2024-12-03 13:07:00 +0100

    feat: Day 03 2024

---
+++ AdventOfCode.Core/Daily2024/Day03/Day03Ex.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using AdventOfCode.Core;
+
+namespace AdventOfCode.Core.Daily2024.Day03
+{
+    public class Day03Ex : Exercise
+    {
+        public override void ComputePart1()
+        {
+            this.Output = ResolveMemory(GetMemory()).ToString();
+        }
+
+        public override void ComputePart2()
+        {
+            this.Output = Regex.Matches($"do(){GetMemory()}don't()", @"do\(\)(.*?)don't\(\)")
+                .Sum(x => ResolveMemory(x.Value))
+                .ToString();
+        }
+
+        private string GetMemory()
+        {
+            // Assuming it's just one line.
+            return string.Join("", this.InputLines);
+        }
+
+        private double ResolveMemory(string memory)
+        {
+            return Regex.Matches(memory, @"mul\([\d]+,[\d]+\)")
+                .Sum(x => ResolveMultiplication(x.Value));
+        }
+
+        private double ResolveMultiplication(string mulPattern)
+        {
+            return Regex.Matches(mulPattern, @"[\d]+")
+                .Select(x => double.Parse(x.Value))
+                .Mult();
+        }
+    }
+}
---
+++ AdventOfCode.Tests/Tests2024/Day03Tests.cs
@@ -0,0 +1,50 @@
+﻿using AdventOfCode.Core.Daily2024.Day03;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Tests.Tests2024
+{
+    internal class Day03Tests
+    {
+        Day03Ex sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            sut = new Day03Ex();
+        }
+
+        [Test]
+        public void Should_do_part_one_from_example()
+        {
+            // GIVEN
+            sut.InputLines = [
+                "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))",
+            ];
+
+            // WHEN
+            sut.ComputePart1();
+
+            // THEN
+            Assert.That(sut.Output, Is.EqualTo("161"));
+        }
+
+        [Test]
+        public void Should_do_part_two_from_example()
+        {
+            // GIVEN
+            sut.InputLines = [
+                "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+don't()mul(32,64](mul(11,8)undo()?mul(8,5))",
+            ];
+
+            // WHEN
+            sut.ComputePart2();
+
+            // THEN
+            Assert.That(sut.Output, Is.EqualTo("48"));
+        }
+    }
+}
