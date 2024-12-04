410948444da72679970f0dfd0557869a4f48667d
Servan42 <charlot.servan@gmail.com>
2024-12-05 00:15:21 +0100

    feat: Day 04 2024

---
+++ AdventOfCode.Core/Daily2024/Day04/Day04Ex.cs
@@ -0,0 +1,95 @@
+﻿using AdventOfCode.Core.Daily2023.Day13;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Core.Daily2024.Day04
+{
+    public class Day04Ex : Exercise
+    {
+        private List<(int x, int y)> COOD_AROUND_SQUARE_8 = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)];
+        private List<(int x, int y)> COOD_AROUND_DIAGONAL = [(-1, -1), (-1, 1), (1, -1), (1, 1)];
+
+        public override void ComputePart1()
+        {
+            this.Output = CountXmas(CountAroundX, 'X').ToString();
+        }
+
+        public override void ComputePart2()
+        {
+            this.Output = CountXmas(CountAroundA, 'A').ToString();
+        }
+
+        private int CountXmas(Func<int, int, int> countAround, char baseChar)
+        {
+            int sum = 0;
+            for (int row = 0; row < this.InputLines.Count; row++)
+            {
+                for (int column = 0; column < this.InputLines[0].Length; column++)
+                {
+                    if (this.InputLines[row][column] != baseChar)
+                        continue;
+
+                    sum += countAround(row, column);
+                }
+            }
+            return sum;
+        }
+
+        private int CountAroundX(int row, int column)
+        {
+            int sum = 0;
+            int maxRowIndex = this.InputLines.Count - 1;
+            int maxColumnIndex = this.InputLines[0].Length - 1;
+            foreach (var cood in COOD_AROUND_SQUARE_8)
+            {
+                if ((row + 3 * cood.x) > maxRowIndex
+                    || (row + 3 * cood.x) < 0
+                    || (column + 3 * cood.y) > maxColumnIndex
+                    || (column + 3 * cood.y) < 0
+                    )
+                    continue;
+
+                if (this.InputLines[row + 1 * cood.x][column + 1 * cood.y] == 'M'
+                    && this.InputLines[row + 2 * cood.x][column + 2 * cood.y] == 'A'
+                    && this.InputLines[row + 3 * cood.x][column + 3 * cood.y] == 'S')
+                {
+                    sum++;
+                }
+            }
+
+            return sum;
+        }
+
+        private int CountAroundA(int row, int column)
+        {
+            int masCount = 0;
+            int maxRowIndex = this.InputLines.Count - 1;
+            int maxColumnIndex = this.InputLines[0].Length - 1;
+            foreach (var cood in COOD_AROUND_DIAGONAL)
+            {
+                if ((row + cood.x) > maxRowIndex
+                    || (row + cood.x) < 0
+                    || (row - cood.x) > maxRowIndex
+                    || (row - cood.x) < 0
+                    || (column + cood.y) > maxColumnIndex
+                    || (column + cood.y) < 0
+                    || (column - cood.y) > maxColumnIndex
+                    || (column - cood.y) < 0
+                    )
+                    continue;
+
+                if (this.InputLines[row + cood.x][column + cood.y] == 'M'
+                    && this.InputLines[row - cood.x][column - cood.y] == 'S')
+                {
+                    masCount++;
+                }
+            }
+            return masCount == 2 ? 1 : 0;
+        }
+    }
+}
---
+++ AdventOfCode.Tests/Tests2024/Day04Tests.cs
@@ -0,0 +1,234 @@
+﻿using AdventOfCode.Core.Daily2024.Day04;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Tests.Tests2024
+{
+    internal class Day04Tests
+    {
+        Day04Ex sut;
+
+        [SetUp]
+        public void Part1_Setup()
+        {
+            sut = new Day04Ex();
+        }
+
+        [TestCase("..XMAS...", "1")]
+        [TestCase("..XMAS.....XMAS...", "2")]
+        [TestCase("..XMASXMAS...XMA", "2")]
+        public void Part1_Should_find_XMAS_on_line(string line, string expectedCount)
+        {
+            // GIVEN
+            sut.InputLines = [ line ];
+
+            // WHEN
+            sut.ComputePart1();
+
+            // THEN
+            Assert.That(sut.Output, Is.EqualTo(expectedCount));
+        }
+
+        [TestCase("..SAMX...", "1")]
+        [TestCase("..SAMX.....SAMX...", "2")]
+        [TestCase("..SAMXSAMX...SAM", "2")]
+        public void Part1_Should_find_XMAS_on_line_reversed(string line, string expectedCount)
+        {
+            // GIVEN
+            sut.InputLines = [ line ];
+
+            // WHEN
+            sut.ComputePart1();
+
+            // THEN
+            Assert.That(sut.Output, Is.EqualTo(expectedCount));
+        }
+
+        [TestCase("..SAMXMAS...", "2")]
+        [TestCase("..SAMXMAS.....SAMXMAS...", "4")]
+        [TestCase("..SAMXSAMXMAS...SAM", "3")]
+        public void Part1_Should_find_XMAS_on_line_reverse_and_overlap(string line, string expectedCount)
+        {
+            // GIVEN
+            sut.InputLines = [line];
+
+            // WHEN
+            sut.ComputePart1();
+
+            // THEN
+            Assert.That(sut.Output, Is.EqualTo(expectedCount));
+        }
+
+        [Test]
+        public void Part1_Should_find_XMAS_on_column()
+        {
+            // GIVEN
+            sut.InputLines = [
+                    "..X...",
+                    "..M.X.",
+                    "..A.M.",
+                    "..S.A.",
+                    "....S.",
+                    "..X...",
+                    "..M...",
+                    "..A...",
+                    "..S...",
+                ];
+
+            // WHEN
+            sut.ComputePart1();
+
+            // THEN
+            Assert.That(sut.Output, Is.EqualTo("3"));
+        }
+
+        [Test]
+        public void Part1_Should_find_XMAS_on_column_reversed()
+        {
+            // GIVEN
+            sut.InputLines = [
+                    "..S...",
+                    "..A.S.",
+                    "..M.A.",
+                    "..X.M.",
+                    "....X.",
+                    "..S...",
+                    "..A...",
+                    "..M...",
+                    "..X...",
+                ];
+
+            // WHEN
+            sut.ComputePart1();
+
+            // THEN
+            Assert.That(sut.Output, Is.EqualTo("3"));
+        }
+
+        [Test]
+        public void Part1_Should_find_XMAS_on_column_reversed_and_overlap()
+        {
+            // GIVEN
+            sut.InputLines = [
+                    "..S...",
+                    "..A.S.",
+                    "..M.A.",
+                    "..X.M.",
+                    "..M.X.",
+                    "..A...",
+                    "..S...",
+                    "..M...",
+                    "..X...",
+                ];
+
+            // WHEN
+            sut.ComputePart1();
+
+            // THEN
+            Assert.That(sut.Output, Is.EqualTo("3"));
+        }
+
+        [Test]
+        public void Part1_Should_find_XMAS_on_diagonal()
+        {
+            // GIVEN
+            sut.InputLines = [
+                    ".X.....",
+                    "..M....",
+                    "...A...",
+                    "....S..",
+                ];
+
+            // WHEN
+            sut.ComputePart1();
+
+            // THEN
+            Assert.That(sut.Output, Is.EqualTo("1"));
+        }
+
+        [Test]
+        public void Part1_Should_do_part_one_from_example()
+        {
+            // GIVEN
+            sut.InputLines = [
+                "MMMSXXMASM",
+                "MSAMXMSMSA",
+                "AMXSXMAAMM",
+                "MSAMASMSMX",
+                "XMASAMXAMM",
+                "XXAMMXXAMA",
+                "SMSMSASXSS",
+                "SAXAMASAAA",
+                "MAMMMXMMMM",
+                "MXMXAXMASX"
+            ];
+
+            // WHEN
+            sut.ComputePart1();
+
+            // THEN
+            Assert.That(sut.Output, Is.EqualTo("18"));
+        }
+
+        //[Test]
+        //public void Should_do_part_two_from_easy_example()
+        //{
+        //    // GIVEN
+        //    sut.InputLines = [
+        //        "M.S",
+        //        ".A.",
+        //        "M.S"
+        //    ];
+
+        //    // WHEN
+        //    sut.ComputePart1();
+
+        //    // THEN
+        //    Assert.That(sut.Output, Is.EqualTo("1"));
+        //}
+
+        //[Test]
+        //public void Should_do_part_two_from_easy_example_2()
+        //{
+        //    // GIVEN
+        //    sut.InputLines = [
+        //        "S.M",
+        //        ".A.",
+        //        "S.M"
+        //    ];
+
+        //    // WHEN
+        //    sut.ComputePart1();
+
+        //    // THEN
+        //    Assert.That(sut.Output, Is.EqualTo("1"));
+        //}
+
+        [Test]
+        public void Should_do_part_two_from_example()
+        {
+            // GIVEN
+            sut.InputLines = [
+                ".M.S......",
+                "..A..MSMS.",
+                ".M.S.MAA..",
+                "..A.ASMSM.",
+                ".M.S.M....",
+                "..........",
+                "S.S.S.S.S.",
+                ".A.A.A.A..",
+                "M.M.M.M.M.",
+                ".........."
+            ];
+
+            // WHEN
+            sut.ComputePart2();
+
+            // THEN
+            Assert.That(sut.Output, Is.EqualTo("9"));
+        }
+    }
+}
